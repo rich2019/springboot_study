763dfd3b0d010c6d6e38d9ad5172ae91474de00b
chendongdong <1327571308@qq.com>
2020-01-17 16:48:27 +0800

    springboot的yml配置文件使用

---
+++ 02-config/src/main/java/com/kuangshen/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.kuangshen.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ 02-config/src/test/java/com/kuangshen/ApplicationTests.java
@@ -0,0 +1,25 @@
+package com.kuangshen;
+
+import com.kuangshen.pojo.Dog;
+import com.kuangshen.pojo.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Autowired
+    private Dog dog;
+    @Autowired
+    private Person person;
+
+    @Test
+    void contextLoads() {
+        System.out.println(dog);//Dog{name='旺财', age=3}
+        System.out.println(person);
+        //Person{name='chendong', age=18, happy=true, birth=Sat Aug 18 00:00:00 CST 1888,
+        // maps={k1=v1, k2=v2}, lists=[code, music, girl], dog=Dog{name='wangcai', age=5}}
+    }
+
+}
