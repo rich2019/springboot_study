b3a036ed8be1860348465517c6986de5f8964057
chendongdong <1327571308@qq.com>
2020-04-20 14:09:34 +0800

    添加springsecurity

---
+++ springsecurity/src/main/java/com/kuang/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.kuang.springsecurity.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//springSecurity配置类
+//AOP  :  拦截器
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //授权
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //首页所有人都可以访问,功能页只有对应权限的人才能访问
+        //请求授权的规则
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("vip1")
+                .antMatchers("/level2/**").hasRole("vip2")
+                .antMatchers("/level3/**").hasRole("vip3");
+
+        //没有权限默认跳转登陆界面,开启登陆界面
+        http.formLogin();
+
+        //注销,跳转到首页
+        http.logout().logoutUrl("/");
+    }
+
+    //认证
+    //密码编码PassWordEncoder
+    //在spring security5.0+ 新增了很多加密方法
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //从内存中加载用户,代替从数据库中查询
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("kuang").password(new BCryptPasswordEncoder().encode("123")).roles("vip1","vip2")
+                .and()
+                .withUser("root").password(new BCryptPasswordEncoder().encode("123")).roles("vip1","vip2","vip3")
+                .and()
+                .withUser("test").password(new BCryptPasswordEncoder().encode("123")).roles("vip3");
+    }
+}
